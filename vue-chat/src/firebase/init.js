5c4eb30b6880d24fece1c898081372b4412189b1
jpromanonet <programming@jpromano.net>
2020-07-29 00:16:05 -0300

    firebase

---
+++ vue-chat/src/firebase/init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "vuejschat-5c731.firebaseapp.com",
+  databaseURL: "https://vuejschat-5c731.firebaseio.com",
+  projectId: "vuejschat-5c731",
+  storageBucket: "vuejschat-5c731.appspot.com",
+  messagingSenderId: "11485340356",
+  appId: "1:11485340356:web:b38febf96f717834a79f91",
+  measurementId: "G-ZNLN2XBR6V",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
