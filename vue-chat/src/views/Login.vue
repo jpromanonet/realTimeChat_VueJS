1a0744f75116c487dcb9b01621b028a6bdd0bbf2
jpromanonet <programming@jpromano.net>
2020-08-03 20:46:20 -0300

    Login view

---
+++ vue-chat/src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <div class="card login">
+      <div class="card-body">
+        <h2 class="card-title text-center">Login</h2>
+      </div>
+    </div>
+  </div>
+</template>
