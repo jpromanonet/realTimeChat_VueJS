0262bbf2334358139d89c46607ee746ff6cc3e7a
jpromanonet <programming@jpromano.net>
2020-08-08 19:31:49 -0300

    New Form

--- vue-chat/src/views/Login.vue
+++ vue-chat/src/views/Login.vue
@@ -3,6 +3,7 @@
     <div class="card login">
       <div class="card-body">
         <h2 class="card-title text-center">Login</h2>
+        <form @submit.prevent="login" class="text-center"></form>
       </div>
     </div>
   </div>
