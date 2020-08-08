1d42e32b090ebce22c5ac97e50e3e4ff9a8670eb
jpromanonet <programming@jpromano.net>
2020-08-08 19:36:57 -0300

    New Form

--- vue-chat/src/views/Login.vue
+++ vue-chat/src/views/Login.vue
@@ -5,7 +5,14 @@
         <h2 class="card-title text-center">Login</h2>
         <form @submit.prevent="login" class="text-center"></form>
         <div class="form-group">
-          <input type="text" class="form-control" placeholder=""
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Please, enter your name"
+          />
+          <p v-if="errorText" class="text-danger">
+            {{ errorText }}
+          </p>
         </div>
       </div>
     </div>
